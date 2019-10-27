b4a478340a3672b31100ee0c3020336c7eb09708
vikytrusowa <vikytrusowa.@gmail.com>
2019-10-27 12:31:47 +0200

    editing page

--- js/main.js
+++ js/main.js
@@ -1,36 +1,27 @@
 $(document).on('click', 'a', function (event) {
-
     event.preventDefault();
-
+});
+$('.header__cost[href^="#"]').click(function () {
+    var target = $(this).attr('href');
+    $('html, body').animate({scrollTop: $(target).offset().top}, 1000);
+    return false;
 });
 jQuery(document).ready(function ($) {
     $('.pop').magnificPopup({
         type: 'inline',
-        mainClass: 'my-mfp-zoom-in'
+        mainClass: 'my-mfp-zoom-in',
     });
 });
-// var active = $('.quiz__element').attr('data-src');
-var element = $('.quiz__content').length;
-
+$(".thank__link").on("click",function () {
+    var magnificPopup = $.magnificPopup.instance;
+    magnificPopup.close();
+});
 $(".quiz__button").click(function () {
-
     var question = $(this).attr('data-src');
-
-    if (question < 6) {
+    if (question < 7) {
         var current = '#question_' + question;
         var next = '#question_' + (parseInt(question) + 1);
         $(current).addClass('quiz__element--hidden');
         $(next).removeClass('quiz__element--hidden')
     }
-
-    // if(active == $('.quiz__element').attr('data-src') ){
-    //     $('.quiz__element').eq(active++).removeClass('quiz__element--hidden');
-    // }
-
-
 });
-
-// var activeStep = 1;
-// $('.quiz__element').eq(activeStep++).removeClass("quiz__element--hidden");
-// if(activeStep == $('.quiz__element').length) $(this).addClass('quiz__element--hidden');
-// console.log(activeStep)
